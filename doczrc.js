897fb95711f230276e4d57608211f2e7458112f8
zhangshuai <zhangshuai08@sunlands.com>
2020-11-29 12:14:47 +0800

    feat:first commit

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+/**
+ * @description: 
+ * @author: zs
+ * @Date: 2020-11-28 12:02:37
+ * @LastEditTime: 2020-11-29 12:10:04
+ * @LastEditors: zs
+ */
+export default {
+  files: './components/**/*.{md,markdown,mdx}', // 识别的文件后缀
+  dest: 'doc-site', // 打包出来的文件目录名
+  title: 'happy-ui', // 站点标题
+  typescript: true, // 组件源文件是通过typescript开发，需要打开此选项
+};
